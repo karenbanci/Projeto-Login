9af20371dac9b8ca372d3603957427edbe724cd8
Karen Banci <karenbanci1@gmail.com>
2021-10-02 01:31:07 -0300

    Arrumei o botão de visualização de senha, fixes #1

--- Projeto Login/js/index.js
+++ Projeto Login/js/index.js
@@ -3,17 +3,20 @@ function showPassword() {
     const eyeSlash = document.getElementById('eye-slash');
     const fieldPassword = document.getElementById('field-password');
 
-    if(eye.style.display === 'none') {
+    if(eyeSlash.style.display !== 'none') {
+      //  ao clicar o botão do olhinho, minha senha se torna visível
       eye.style.display = 'block';
       eyeSlash.style.display = 'none';
       fieldPassword.type = 'text';
     } else {
+      //se eu digitar a senha, ela não vai estar visível
     eye.style.display = 'none';
     eyeSlash.style.display = 'block';
     fieldPassword.type = 'password';
     }
 };
 
+// ao clicar o botão de login, ele vai aparecer uma caixa de alerta dizendo que está logado
 document.getElementById('btn-login').addEventListener('click' , function(e){
     e.preventDefault();
     alert('logado');
